9a53f9384d6e4f706c118dcfaa9f1837c852cbb7
rodolfoms <rodolfoms@gmail.com>
2021-07-06 11:51:07 +0100

    end of seaction 5 react crud

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+
 
 ReactDOM.render(
   <React.StrictMode>
